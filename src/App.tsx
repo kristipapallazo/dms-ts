64afba60e195811c7ac67d1f210434e5b82a2a85
kristi papallazo <papallazokristi@gmail.com>
2023-12-04 18:29:52 +0100

    .

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,37 @@ import React from 'react';
 import './App.css';
 
 function App() {
+  const fetchData = async () => {
+    const STATIC_URL = "http://localhost:8101/dms-app/all-scripts";
+    try {
+      const app = "test"; // Replace with your app name
+
+      const url = `${STATIC_URL}/${app}`
+      const devel = true
+      const response = await fetch(url, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          // Add any other headers if needed
+        },
+        body: JSON.stringify({ devel }),
+      });
+
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+
+      const data = await response.json();
+      console.log(data);
+      // Handle the response data here
+    } catch (error) {
+      console.error("Error fetching data:", error);
+      // Handle errors here
+    }
+  };
   return (
     <div className="app">
-      
+      <button onClick={fetchData}>request</button>
     </div>
   );
 }
